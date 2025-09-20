af9397460c3636cccb079566ef4d987cb3de9615
Frano Perotti <francojoseperotti@gmail.com>
2025-09-20 14:22:25 -0300

    feat(polybar): add launch_polybar.sh script for multi-monitor Polybar initialization

---
+++ scripts/.local/bin/launch_polybar.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+killall -q polybar
+
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload &
+done
