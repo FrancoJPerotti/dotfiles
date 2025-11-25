8f127a49da6883b22fa428f174d1c46885843c3b
Franco Perotti <franco.perotti@eclypsium.com>
2025-11-24 23:33:28 -0300

    feat(nvim): configure huez colors

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,7 @@
 return {
 	{
 		"folke/tokyonight.nvim",
-		lazy = false, -- make sure we load this during startup if it is your main colorscheme
-		priority = 1000, -- make sure to load this before all the other start plugins
-		config = function()
-			-- load the colorscheme here
-			vim.cmd([[colorscheme tokyonight]])
-		end,
+		lazy = false, -- ensure the theme is ready before Huez applies it
+		priority = 1000,
 	},
 }
